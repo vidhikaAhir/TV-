e3dc7efb381d97608c12a0b4dc54bc6c0362d035
vidhika <vidhika.a@gmail.com>
2026-05-11 16:34:04 +0530

    Added Watchlist, search

---
+++ TV+/Screens/MovieDetails.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetails.swift
+//  TV+
+//
+//  Created by neo on 5/11/26.
+//
+
+import SwiftUI
+
+struct MovieDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetails()
+}
---
+++ TV+/Screens/WatchListScreen.swift
@@ -0,0 +1,18 @@
+//
+//  WatchList.swift
+//  TV+
+//
+//  Created by neo on 5/11/26.
+//
+
+import SwiftUI
+
+struct WatchList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WatchList()
+}
